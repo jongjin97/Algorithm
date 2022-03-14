c760e72106c6b7981edc71b0ac17350ab60baed6
jongjin97 <kakao1302@naver.com>
2022-03-14 11:08:28 +0900

    A/B 1008

---
+++ Algorithm/src/baekjoon/AB_1008.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class AB_1008 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		double a= sc.nextDouble();
+		double b= sc.nextDouble();
+		
+		System.out.println(a/b);
+	}
+
+}
