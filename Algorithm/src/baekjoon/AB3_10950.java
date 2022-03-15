06b274914d5c47763fd12cc3740f1dd61c9545d0
jongjin97 <kakao1302@naver.com>
2022-03-15 10:47:27 +0900

    A+B-3 10950

---
+++ Algorithm/src/baekjoon/AB3_10950.java
@@ -0,0 +1,20 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class AB3_10950 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		int test = sc.nextInt();
+
+		for (int i = 0; i < test; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			System.out.println(a+b);
+		}
+	}
+
+}
