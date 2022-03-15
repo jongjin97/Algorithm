abd1328ac13bf03566d9636df382809e1a7d9240
jongjin97 <kakao1302@naver.com>
2022-03-15 10:31:32 +0900

    사칙연산 10869

---
+++ Algorithm/src/baekjoon/Arithmetic_Operation_10869.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Arithmetic_Operation_10869 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+	}
+
+}
