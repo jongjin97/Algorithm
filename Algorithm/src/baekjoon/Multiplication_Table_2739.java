9dbcd278c727a162fa7795f322b72fd7b45257b9
jongjin97 <kakao1302@naver.com>
2022-03-15 10:35:18 +0900

    구구단 2739

---
+++ Algorithm/src/baekjoon/Multiplication_Table_2739.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Multiplication_Table_2739 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int num =  sc.nextInt();
+		
+		for(int i=1; i<10; i++) {
+			System.out.println(num + " * " + i + " = " + num*i);
+		}
+	}
+
+}
