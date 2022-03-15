b08aa2e47e7c5053b3d439bfbe7ce2ee44753a58
jongjin97 <kakao1302@naver.com>
2022-03-15 10:45:26 +0900

    윤년 2753

---
+++ Algorithm/src/baekjoon/Leap_Year_2753.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Leap_Year_2753 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int year = sc.nextInt();
+		
+		if(year%4 == 0 && year%100 != 0 | year%400 == 0)
+			System.out.println(1);
+		else
+			System.out.println(0);
+	}
+
+}
