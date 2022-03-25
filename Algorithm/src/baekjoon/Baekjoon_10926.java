e34d10b9ee833b9662e9de6a3eb780a094caa545
jongjin97 <kakao1302@naver.com>
2022-03-25 21:30:12 +0900

    ??! 10926

---
+++ Algorithm/src/baekjoon/Baekjoon_10926.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon_10926 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		
+		System.out.println(str+"??!");
+	}
+
+}
