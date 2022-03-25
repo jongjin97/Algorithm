61d0c2be24b2ef009e027f7e75fd46b16e7207ba
jongjin97 <kakao1302@naver.com>
2022-03-25 22:08:00 +0900

    빠른 A+B 15552

---
+++ Algorithm/src/baekjoon/Faster_AB_15552.java
@@ -0,0 +1,31 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Faster_AB_15552 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int num = Integer.parseInt(br.readLine());
+		for(int i=0; i<num; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			String str = Integer.toString(a+b);
+			bw.write(str);
+			bw.newLine();
+		}
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+
+}
