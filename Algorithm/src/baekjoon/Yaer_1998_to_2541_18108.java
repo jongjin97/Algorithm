cd078ad1f4553e6fada7da9244b6301d64064445
jongjin97 <kakao1302@naver.com>
2022-03-25 21:32:27 +0900

    1998년생인 내가 태국에서는 2541년생?! 18108

---
+++ Algorithm/src/baekjoon/Yaer_1998_to_2541_18108.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Yaer_1998_to_2541_18108 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		
+		System.out.println(num-543);
+	}
+
+}
