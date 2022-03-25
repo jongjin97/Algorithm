b416daee3189e4f9ca72ff04d2bc0704afecf8e7
jongjin97 <kakao1302@naver.com>
2022-03-25 21:35:37 +0900

    시험 성적 9498

---
+++ Algorithm/src/baekjoon/Test_Score_9498.java
@@ -0,0 +1,25 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Test_Score_9498 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		if(num >= 90)
+			System.out.println("A");
+		else if(num >= 80)
+			System.out.println("B");
+		else if(num >= 70)
+			System.out.println("C");
+		else if(num >= 60)
+			System.out.println("D");
+		else
+			System.out.println("F");
+	}
+
+}
