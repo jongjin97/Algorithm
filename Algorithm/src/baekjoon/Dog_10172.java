f5888eff05a67dc48b36769a73203f3b7711731e
jongjin97 <kakao1302@naver.com>
2022-03-25 21:28:23 +0900

    개 10172

---
+++ Algorithm/src/baekjoon/Dog_10172.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+public class Dog_10172 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.err.println("|\\_/|");
+		System.err.println("|q p|   /}");
+		System.err.println("( 0 )\"\"\"\\");
+		System.err.println("|\"^\"`    |");
+		System.err.println("||_/=\\\\__|");
+	}
+
+}
