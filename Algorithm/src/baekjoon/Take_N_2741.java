9fa919cb79701b8694fa54028e2a3d0480c92a36
jongjin97 <kakao1302@naver.com>
2022-03-25 22:09:49 +0900

    N 찍기 2741

---
+++ Algorithm/src/baekjoon/Take_N_2741.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Take_N_2741 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		for(int i=1; i<=num; i++) {
+			System.out.println(i);
+		}
+	}
+
+}
