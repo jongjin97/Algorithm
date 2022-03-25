79149f759356ecc3fc3ed356086252945a417e65
jongjin97 <kakao1302@naver.com>
2022-03-25 22:11:37 +0900

    기찍 N 2742

---
+++ Algorithm/src/baekjoon/Reverse_Take_N_2742.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Reverse_Take_N_2742 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		for(int i=num; i>0; i--) {
+			System.out.println(i);
+		}
+	}
+
+}
