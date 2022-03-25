3cf51269bc35db861f143e12aead599895e33f74
jongjin97 <kakao1302@naver.com>
2022-03-25 22:15:55 +0900

    A+B-8 11022

---
+++ Algorithm/src/baekjoon/AB_8_11022.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class AB_8_11022 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int test = sc.nextInt();
+		
+		for(int i=1; i<=test; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int sum = a+b;
+			
+			System.out.println("Case #"+i+": " + a + " + " + b + " = " +sum);
+		}
+	}
+
+}
