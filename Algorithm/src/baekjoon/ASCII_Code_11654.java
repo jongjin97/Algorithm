35c1f3babd4a859f15fe52a9b4f738ad885eca19
jongjin97 <kakao1302@naver.com>
2022-03-26 20:56:01 +0900

    아스키 코드 11654

---
+++ Algorithm/src/baekjoon/ASCII_Code_11654.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class ASCII_Code_11654 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.next();
+		int n = str.charAt(0);
+		
+		System.out.println(n);
+	}
+
+}
