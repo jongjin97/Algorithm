00a27cc1145f23a779afd6180404f920dd8935b9
jongjin97 <kakao1302@naver.com>
2022-03-29 21:27:08 +0900

    택시 기하학 3053

---
+++ Algorithm/src/baekjoon/Taxi_Geometry_3053.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Taxi_Geometry_3053 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double pi = Math.PI;
+		Scanner sc = new Scanner(System.in);
+		
+		double num = sc.nextDouble();
+		
+		System.out.printf("%.6f\n", num*num*pi);
+		System.out.printf("%.6f", num*num*2);
+	}
+
+}
