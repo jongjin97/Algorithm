f1de630ccc9785e08193388bb65f83600dc61543
jongjin97 <kakao1302@naver.com>
2022-04-22 20:17:28 +0900

    숫자 카드 10815

---
+++ Algorithm/src/baekjoon/Number_Card_10815.java
@@ -0,0 +1,56 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Number_Card_10815 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] num1 = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            num1[i] = Integer.parseInt(st.nextToken());
+        }
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        int[] num2 = new int[M];
+        for(int i=0; i<M; i++){
+            num2[i] = Integer.parseInt(st.nextToken());
+        }
+        br.close();
+        Arrays.sort(num1);
+        for(int i=0; i<M; i++){
+            int lo = 0;
+            int hi = N-1;
+            int mid = (lo+hi)/2;
+
+            while(true){
+                if(num2[i] == num1[mid] || num2[i] == num1[lo] || num2[i] == num1[hi]){
+                    bw.write("1");
+                    bw.newLine();
+                    break;
+                } else if(lo + 1 == hi){
+                    bw.write("0");
+                    bw.newLine();
+                    break;
+                }
+
+
+                if(num2[i] > num1[mid]){
+                    lo = mid;
+                    mid = (lo+hi)/2;
+                } else if(num2[i] < num1[mid]){
+                    hi = mid;
+                    mid = (lo+hi)/2;
+                }
+            }
+        }
+        bw.flush();
+        bw.close();
+    }
+}
