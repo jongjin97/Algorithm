2b745cd66f6c8b5363578091ad96c84c0c7f7175
jongjin97 <kakao1302@naver.com>
2023-02-21 22:48:41 +0900

    단어 길이 재기 2743

---
+++ Algorithm/src/baekjoon/Measure_word_length2743.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.io.*;
+
+public class Measure_word_length2743 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        System.out.println( str.length() );
+    }
+}
