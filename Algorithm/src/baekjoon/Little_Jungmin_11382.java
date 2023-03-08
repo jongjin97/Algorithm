bd253a88a794387d38cf75403e89287eb39f48ea
jongjin97 <kakao1302@naver.com>
2023-03-08 21:38:39 +0900

    꼬마 정민 11382

---
+++ Algorithm/src/baekjoon/Little_Jungmin_11382.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Little_Jungmin_11382 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(sc.nextLong() + sc.nextLong() + sc.nextLong());
+    }
+}
