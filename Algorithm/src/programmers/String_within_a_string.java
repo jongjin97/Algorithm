8fa49a530f8570aaa714d0edb41030261c7fa3e5
jongjin97 <kakao1302@naver.com>
2023-03-29 02:13:57 +0900

    문자열안에 문자열

---
+++ Algorithm/src/programmers/String_within_a_string.java
@@ -0,0 +1,13 @@
+package programmers;
+
+public class String_within_a_string {
+
+    public int solution(String str1, String str2) {
+        int answer = 0;
+
+        if(str1.contains(str2))
+            return 1;
+
+        return 2;
+    }
+}
