f52a9bfc126f0dc5a96aa4287bfee896f2554bcc
jongjin97 <kakao1302@naver.com>
2023-03-29 23:08:32 +0900

    인덱스 바꾸기

---
+++ Algorithm/src/programmers/Change_index.java
@@ -0,0 +1,19 @@
+package programmers;
+
+public class Change_index {
+
+    public String solution(String my_string, int num1, int num2) {
+        String answer = "";
+
+        String[] str = my_string.split("");
+
+        String tmp = str[num1];
+        str[num1] = str[num2];
+        str[num2] = tmp;
+
+        for(String s : str)
+            answer += s;
+
+        return answer;
+    }
+}
