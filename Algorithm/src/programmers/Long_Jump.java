9253e1570e68fbf7119be3e8b703b3c1370d9606
jongjin97 <kakao1302@naver.com>
2023-07-23 21:12:26 +0900

    멀리 뛰기

--- Algorithm/src/programmers/Long_Jump.java
+++ Algorithm/src/programmers/Long_Jump.java
@@ -1,2 +1,17 @@
-package programmers;public class Long_Jump {
+package programmers;
+
+public class Long_Jump {
+    public long solution(int n) {
+        long answer = 0;
+
+        int[] dp = new int[Math.max(n+1, 2)];
+        dp[1] = 1;
+        if(dp.length>2)
+            dp[2] = 2;
+        for(int i=3; i<dp.length; i++){
+            dp[i] = (dp[i-1] + dp[i-2])%1234567;
+        }
+        answer = dp[n];
+        return answer;
+    }
 }
