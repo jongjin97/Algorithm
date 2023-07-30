e1634f86144e2508d912b1345bd7d55d28711f1f
jongjin97 <kakao1302@naver.com>
2023-07-30 16:59:38 +0900

    두 원 사이의 정수 쌍

---
+++ Algorithm/src/programmers/Pairs_of_integers_between_two_circles.java
@@ -0,0 +1,17 @@
+package programmers;
+
+public class Pairs_of_integers_between_two_circles {
+
+    public long solution(int r1, int r2) {
+        long answer = 0;
+        for (int i=1; i<=r2; i++) {
+            long minJ = (int) Math.ceil(Math.sqrt(1.0*r1*r1 - 1.0*i*i));
+            long maxJ = (int) Math.floor(Math.sqrt(1.0*r2*r2 - 1.0*i*i));
+
+            answer += (maxJ - minJ + 1);
+
+        }
+        return answer*4;
+    }
+
+}
