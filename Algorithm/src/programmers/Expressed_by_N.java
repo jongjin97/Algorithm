7149d972d4cf3ba864c760309e6b63e1c624a3ee
jongjin97 <kakao1302@naver.com>
2023-08-26 16:26:32 +0900

    N으로 표현

---
+++ Algorithm/src/programmers/Expressed_by_N.java
@@ -0,0 +1,35 @@
+package programmers;
+
+import java.util.*;
+
+public class Expressed_by_N {
+    public int solution(int N, int number) {
+        Map<Integer, Integer> map = initMap(N);
+        rSolution(N, 0, 0, map);
+        return map.getOrDefault(number, -1);
+    }
+
+    private void rSolution(int N, int cnt, int current, Map<Integer, Integer> map) {
+        if (cnt > 8 || (cnt > 0 && current == 0)) {
+            return;
+        }
+
+        map.put(current, Math.min(map.getOrDefault(current, cnt), cnt));
+
+        for (int i = N, digit = 1; i <= 1111111111; i = i * 10 + N, digit++) {
+            rSolution(N, cnt + digit, current + i, map);
+            rSolution(N, cnt + digit, current - i, map);
+            rSolution(N, cnt + digit, current * i, map);
+            rSolution(N, cnt + digit, current / i, map);
+        }
+    }
+
+    private Map<Integer, Integer> initMap(int N) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = N, digit = 1; i <= 1111111111; i = i * 10 + N, digit++) {
+            map.put(i, digit);
+        }
+
+        return map;
+    }
+}
