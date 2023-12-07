a1bb09b7a19720a8c90d3647d190580260870e9c
jongjin97 <kakao1302@naver.com>
2023-12-07 20:38:43 +0900

    문자와 문자열

---
+++ Algorithm/src/baekjoon/Char_and_String_27866.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Char_and_String_27866 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int index = sc.nextInt();
+        System.out.println(str.charAt(index - 1));
+    }
+}
