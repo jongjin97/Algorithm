5e385c6801f1ed4a96ab3626da014c4b705601dc
jongjin97 <kakao1302@naver.com>
2023-12-27 00:28:44 +0900

    Z

---
+++ Algorithm/src/baekjoon/Z_1074.java
@@ -0,0 +1,43 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Z_1074 {
+    public static int N,r,c;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        int size = (int) Math.pow(2, N);
+
+        bw.write(find(size, r, c) + "\n");
+
+        bw.flush();
+
+        bw.close();
+    }
+
+    public static int find(int size, int r, int c) {
+        if(size == 1) return 0;
+
+        // 1사분면
+        if(r < size/2 && c < size/2) {
+            return find(size/2, r, c);
+        }
+        // 2사분면
+        if(r < size/2 && c >= size/2) {
+            return find(size/2, r, c-size/2) + (size * size / 4);
+        }
+        // 3사분면
+        if(r >= size/2 && c < size/2) {
+            return find(size/2, r-size/2, c) + (size * size / 4 * 2);
+        }
+        // 4사분면
+        return find(size/2, r-size/2, c-size/2) + (size * size / 4 * 3);
+    }
+}
