60f175af30a45b1f623765c948df3b5609a5ec54
jongjin97 <kakao1302@naver.com>
2024-04-11 21:01:36 +0900

    녹색거탑

---
+++ Algorithm/src/baekjoon/green_tower_24723.java
@@ -0,0 +1,11 @@
+package baekjoon;
+
+import java.util.Scanner;
+//녹색거탑
+public class green_tower_24723 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println((int) Math.pow(2, N));
+    }
+}
