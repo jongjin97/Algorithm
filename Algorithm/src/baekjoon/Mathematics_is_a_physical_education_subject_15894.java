7e840002e9489fb7dfe473c20bdfba3c6c558011
jongjin97 <kakao1302@naver.com>
2024-06-25 15:37:54 +0900

    수학은 체육과목 입니다.

---
+++ Algorithm/src/baekjoon/Mathematics_is_a_physical_education_subject_15894.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+//수학은 체육과목 입니다
+public class Mathematics_is_a_physical_education_subject_15894 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        System.out.println(n*3+1+n-1);
+    }
+}
