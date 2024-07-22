cb46e0c029238a9ad33031933448fa9699928ae6
jongjin97 <kakao1302@naver.com>
2024-07-22 21:24:09 +0900

    FA

---
+++ Algorithm/src/baekjoon/FA_14935.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class FA_14935 {
+    static int mem;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String N = sc.next();
+
+        System.out.println("FA");
+    }
+}
